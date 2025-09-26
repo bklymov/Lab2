8d733f3abb987b5d7617315fe2ee7dc056ea4f15
Bohdan Klymov <c00315320@setu.ie>
2025-09-26 09:38:13 +0000

    T3

---
+++ T3.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+
+def ip_parse(line):
+
+    tokens = line.split()
+    if "from" in tokens:
+        idx = tokens.index("from")
+        if idx + 1 < len(tokens):
+            return tokens[idx + 1]
+    return None
+
+counts = defaultdict(int)       #create a dictionary to keep track of IPs
+
+with open("sample_auth_small.log") as f:
+    for line in f:
+        if "Failed password" in line or "Invalid user" in line:
+            # extract ip
+            ip = ip_parse(line)
+            if ip:
+                counts[ip] += 1
+print(counts)
+
+import time
+start = time.time()     #run counting
+end = time.time()
+print("Elapsed:", end-start, "seconds")
